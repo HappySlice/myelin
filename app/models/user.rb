500582f4c60a7e54874add98c869b6634019404d
David Busch <david.busch@gmail.com>
2011-09-13 04:22:44 -0700

    User relationships (following/followers)

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,15 @@ class User < ActiveRecord::Base
   
   has_many :microposts, :dependent => :destroy
   
+  has_many :relationships, :foreign_key => "follower_id",
+                           :dependent => :destroy
+  has_many :following, :through => :relationships, :source => :followed
+  
+  has_many :reverse_relationships, :foreign_key => "followed_id",
+                                   :class_name => "Relationship",
+                                   :dependent => :destroy
+  has_many :followers, :through => :reverse_relationships, :source => :follower
+  
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   
   validates :name,  :presence => true,
@@ -23,6 +32,18 @@ class User < ActiveRecord::Base
   
   def feed
     Micropost.where("user_id = ?", id)
+  end
+  
+  def following?(followed)
+    relationships.find_by_followed_id(followed)
+  end
+  
+  def follow!(followed)
+    relationships.create!(:followed_id => followed.id)
+  end
+  
+  def unfollow!(followed)
+    relationships.find_by_followed_id(followed).destroy
   end
   
   class << self
